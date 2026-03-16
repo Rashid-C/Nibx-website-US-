b22eb4dbd7d931d18e4220ce2e1dc0bdb4037055
Rashid-C <anurashid105@gmail.com>
2026-03-16 12:12:36 +0400

    Contact form with web3form api

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Services from "./components/Services";
 import ServiceCard from "./components/ServiceCard";
 import OurWork from "./components/OurWork";
 import Teams from "./components/Teams";
+import ContactUs from "./components/ContactUs";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -14,12 +16,14 @@ const App = () => {
 
   return (
     <div className="dark:bg-black relative">
+      <Toaster />
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
       <TrustedBy />
       <Services />
       <OurWork />
       <Teams />
+      <ContactUs />
     </div>
   );
 };
